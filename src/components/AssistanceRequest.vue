ce1aa53fb7327481cb796482bacb06caa4a7bf05
Ron Northrip <ron.northrip@gmail.com>
2019-10-30 14:42:18 -0400

    First pass at the form layout with the basic parts in place

--- src/components/AssistanceRequest.vue
+++ src/components/AssistanceRequest.vue
@@ -1,7 +1,86 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    
+    <div class="card card-default">
+      <div class="card-header font-weight-bold">{{ title }}</div>
+
+      <div class="card-body">
+        <form role="form">
+            <!-- First Name -->
+            <div class="form-group">
+                <div>
+                    <input type="text" class="form-control" name="first_name" placeholder="First Name">
+                </div>
+                <div class="text-danger text-right mb-2 mt-1" data-for="first_name">
+                    required
+                </div>
+            </div>
+
+            <!-- Last Name -->
+            <div class="form-group">
+                <div>
+                    <input type="text" class="form-control" name="last_name" placeholder="Last Name">
+                </div>
+                <div class="text-danger text-right mb-2 mt-1" data-for="last_name">
+                    required
+                </div>
+            </div>
+
+            <!-- Email Address -->
+            <div class="form-group">
+                <div>
+                    <input type="email" class="form-control" name="email" placeholder="Email Address">
+                </div>
+                <div class="text-danger text-right mb-2 mt-1" data-for="email">
+                    required
+                </div>
+            </div>
+
+            <!-- Service Type -->
+            <div class="form-group">
+                <div>
+                    <select class="form-control" name="service_type">
+                      <option>Select Service Type</option>
+                    </select>
+                </div>
+                <div class="text-danger text-right mb-2 mt-1" data-for="service_type">
+                    required
+                </div>
+            </div>
+
+            <!-- Description -->
+            <div class="form-group">
+                <div>
+                    <textarea class="form-control" name="description" rows="10">
+                    </textarea>
+                </div>
+                <div class="text-danger text-right mb-2 mt-1" data-for="description">
+                    required
+                </div>
+            </div>
+
+            <!-- Accept Terms -->
+            <div class="form-group">
+                <div class="ml-1">
+                    <div class="form-check">
+                        <label class="form-check-label">
+                            <input type="checkbox" name="accept_terms" class="form-check-input"> 
+                            I hereby accept the terms of service for THE NETWORK and the Privacy Policy
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Submit Button -->
+            <div class="form-group mt-3 mb-0 text-right">
+                <div class="">
+                    <button type="submit" class="btn btn-primary">
+                        Get Assistance
+                    </button>
+                </div>
+            </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +90,11 @@ export default {
   props: {
     title: String
   },
+  computed: {
+  },
   mounted() {
+  },
+  methods: {
   }
 }
 </script>
